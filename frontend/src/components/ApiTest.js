0d25465fdbbdd49c8b0245bdd0064ef08bf9aafe
Daniel Knopf <danielkn609@gmail.com>
2021-07-20 15:03:44 -0400

    added react app

---
+++ frontend/src/components/ApiTest.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+function ApiTest() {
+  const [numUsers, setNumUsers] = useState(0);
+
+  axios.get("http://localhost:8000/api/overallStats/").then((response) => {
+    console.log(response);
+  });
+
+  return <div className="App"></div>;
+}
+
+export default ApiTest;
